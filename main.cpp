ed97dcca918aa93f82f40ac8a188ddd48b63fd45
khoai3010 <khanhnh309@gmail.com>
2023-08-03 18:54:41 +0700

    checkmap, random, Vector3 function

--- main.cpp
+++ main.cpp
@@ -9,8 +9,21 @@ using namespace std;
 
 void CreateMap(int a);//creat a map
 void OBJ(int b);//input the information about object
-void play();
-void move(int x, int y, int z, int t);//move the character
+void Play();
+void Move(int x, int y, int z, int t);//move the character
+
+
+struct Vector3
+{
+	int x, y, z;
+	Vector3() {};
+};
+
+Vector3 CheckSpawn(Vector3);
+
+Vector3 RandomInt();
+
+int map[10][10];
 
 int main() {
 	int choose;
@@ -32,7 +45,7 @@ int main() {
 			break;
 		case 2: 
 			system("cls");
-			play();
+			Play();
 			break;
 		case 3:
 			system("cls");
@@ -49,6 +62,16 @@ int main() {
 }
 void OBJ(int b) {
 	
+	Vector3 objPos;
+	objPos.x = RandomInt().x;
+	objPos.y = RandomInt().y;
+	objPos.z = 1;
+	
+	objPos.x = CheckSpawn(objPos).x;
+	objPos.y = CheckSpawn(objPos).y;
+
+	map[objPos.x][objPos.y] = 1;
+
 	ifstream input;
 	fstream output;
 	string OJname;
@@ -58,24 +81,23 @@ void OBJ(int b) {
 	case 1: OJname = "tree";//name of the object
 		output << OJname << endl;
 		output << "C:/Users/GIGABYTE/source/repos/gameBK/tree.obj" << endl;//the path of the file object
-		output << RandomInt() << "," << RandomInt() << "," << 1 << endl;//location
+		output << objPos.x << "," << objPos.y << "," << 1 << endl;//location
 		output << 1 << "," << 1 << "," << 1 << endl;//size
 		output << 0 << "," << 0 << "," << 0 << endl;//swivel angle
 		break;
 	case 2: OJname = "house";
 		output << OJname << endl;
 		output << "C:/Users/GIGABYTE/source/repos/gameBK/house.obj" << endl;//the path of the file object
-		output << "0,1,0" << endl;//location
-		output << "0,0,0" << endl;//size
-		output << "1,1,1" << endl;//swivel angle
+		output << objPos.x << "," << objPos.y << "," << 1 << endl;//location
+		output << 1 << "," << 1 << "," << 1 << endl;//size
+		output << 0 << "," << 0 << "," << 0 << endl;//swivel angle
 		break;
 		
 	}
 	output.close();
 
-
 }
-void play() {
+void Play() {
 	int x, y, z;//coordinates of the character
 	int m;//the map that character in
 	x = 1;
@@ -94,7 +116,7 @@ void play() {
 		if (_kbhit()) {
 			char c = _getch();
 			if (c == 'm') {
-				move(x, y, z, m);
+				Move(x, y, z, m);
 
 			}
 			else if (c == 'e') {
@@ -104,6 +126,7 @@ void play() {
 	}
 	
 }
+
 void CreateMap(int a) {
 	
 	ifstream input;
@@ -114,9 +137,10 @@ void CreateMap(int a) {
 	output.close();
 	OBJ(1);
 	OBJ(2);
-
 }
-void move(int x, int y, int z, int t) {
+
+
+void Move(int x, int y, int z, int t) {
 	cout << "where you want to go?" << endl;
 	cout << "enter the map that you want to go to:";
 	cin >> t;
@@ -129,17 +153,37 @@ void move(int x, int y, int z, int t) {
 	cout << "you are in location " << x << "," << y << "," << z << " in the map " << t << endl;;
 }
 
-//struct Vector3
-//{
-//	int x, y, z;
-//};
 
 
-int RandomInt()
+Vector3 RandomInt()
 {
 	srand(time(0));
-	int random = rand % 10 + 1;
-	return random;
+	Vector3 ranVec;
+	ranVec.x = rand() % 10 + 1;
+	ranVec.y = rand() % 10 + 1;
+	ranVec.z = 1;
+	return ranVec;
+}
+
+
+Vector3 CheckSpawn(Vector3 checkVec)
+{
+	int x, y;
+
+	x = checkVec.x;
+	y = checkVec.y;
+
+	while (map[x][y] != 0)
+	{
+		srand((int)time(0));
+		x = rand() % 10 + 1;
+		y = rand() % 10 + 1;
+	}
+
+	Vector3 ran;
+	ran.x = x;
+	ran.y = y;
+	return ran;
 }
 
 
